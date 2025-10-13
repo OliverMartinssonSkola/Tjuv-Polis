41f92fcf9b965f7eaeef61358709d7399d672b76
wafae Boulahoual <wafae.essbai@gmail.com>
2025-10-13 18:37:06 +0200

    test

---
+++ PolisOchTjuv/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PolisOchTjuv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, System25!");
+        }
+    }
+}
