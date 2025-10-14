661df3e387df0cee9087774d0c95bc0edf57e037
Tobias Kickmaier <t.kickmaier@hotmail.com>
2025-10-14 10:01:44 +0200

    Skapat klassen City Basklassen Person

---
+++ City.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjuvochPolis
+{
+    
+    internal class City
+    {
+        
+        public static void CityRoofFloor()
+        {
+            for (int i = 0; i < 102; i++)
+            {
+                Console.Write('#');
+            }
+
+        }
+        public static void Streets()
+        {
+            {
+                for (int i = 0; i < 102; i++)
+                {
+                    Console.Write('#');
+                }
+
+            }
+            Console.WriteLine();
+            for (int row = 0;row < Program.streets.GetLength(0); row++)
+            {
+                Console.Write('#');
+                for (int col = 0; col < Program.streets.GetLength(1); col++)
+                {
+                    Console.Write(' ');
+                }
+                Console.Write('#');
+                Console.WriteLine();
+            }
+            {
+                for (int i = 0; i < 102; i++)
+                {
+                    Console.Write('#');
+                }
+
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
+        public static char[,] streets = new char[25, 100];
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            //string[] names = { };
+            //City.CityRoofFloor();
+            //Console.WriteLine();
+            City.Streets();
+            //City.CityRoofFloor();
+
         }
     }
 }
