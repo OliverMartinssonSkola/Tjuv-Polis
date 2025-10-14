9ac7b458fdf287c85dd67c591a172a3d37e86a4d
Tobias Kickmaier <t.kickmaier@hotmail.com>
2025-10-14 10:26:17 +0200

    Testar igen att lägga upp

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TjuvochPolis
+{
+    internal class Program
+    {
+        public static char[,] streets = new char[25, 100];
+        static void Main(string[] args)
+        {
+            //string[] names = { };
+            //City.CityRoofFloor();
+            //Console.WriteLine();
+            City.Streets();
+            //City.CityRoofFloor();
+
+        }
+    }
+}
