9923705f7fda326b675ca72262b8722efb82b099
wafae Boulahoual <wafae.essbai@gmail.com>
2025-10-14 10:16:08 +0200

    ny klass skapat

--- PolisOchTjuv/obj/Debug/net9.0/PolisOchTjuv.AssemblyInfo.cs
+++ PolisOchTjuv/obj/Debug/net9.0/PolisOchTjuv.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PolisOchTjuv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f26d1a1eb1234246584c153a5bfcbbaddbed0a97")]
 [assembly: System.Reflection.AssemblyProductAttribute("PolisOchTjuv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PolisOchTjuv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
